a5f614f98c09e6f66b6718aed7db98fac8da0290
dimansidorov <dimkasidorow@gmail.com>
2023-02-15 17:16:11 +0100

    create Item Model, register Item Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
