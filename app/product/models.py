34abb0c57e751ea461769c98442bc8ab3fa21147
dimansidorov <dimkasidorow@gmail.com>
2023-12-21 19:56:44 +0100

    created Item model

--- app/product/models.py
+++ app/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(
+        'Price', max_digits=10, decimal_places=2, default=0, validators=[MinValueValidator(0)]
+        )
+
+    def __str__(self):
+        return self.name
