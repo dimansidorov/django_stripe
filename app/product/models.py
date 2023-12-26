5b49323c70588ba21291dd62fadd736e28736b4b
dimansidorov <dimkasidorow@gmail.com>
2023-12-26 18:33:35 +0100

    update Item model

--- app/product/models.py
+++ app/product/models.py
@@ -5,8 +5,8 @@ from django.core.validators import MinValueValidator
 class Item(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
-    price = models.DecimalField(
-        'Price', max_digits=10, decimal_places=2, default=0, validators=[MinValueValidator(0)]
+    price = models.IntegerField(
+        'Price', validators=[MinValueValidator(0)]
         )
 
     def __str__(self):
